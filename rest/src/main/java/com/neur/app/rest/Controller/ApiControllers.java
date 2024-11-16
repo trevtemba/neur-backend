726fd21c73c8cacfc8834fc55e58166040f274e5
Temba <drxpnotlox@gmail.com>
2024-11-15 21:42:55 -0500

    Added rest cotroller declarations for auth paths

--- rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
+++ rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
@@ -1,13 +1,43 @@
 package com.neur.app.rest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiControllers {
 
-    @GetMapping(value = "/")
-    public String getPage() {
+    @PostMapping(value = "/auth/signup")
+    public String createAccount() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/login")
+    public String loginAccount() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/logout")
+    public String logoutAccount() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/forgot-password")
+    public String forgotPassword() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/reset-password")
+    public String resetPassword() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/reset-password")
+    public String refreshToken() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/auth/verify-email")
+    public String verifyEmail() {
         return "Hello World";
     }
 }
