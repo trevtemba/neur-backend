20b37ed8151f867b30e6ec1ff30a17f87644e41e
Temba <drxpnotlox@gmail.com>
2024-11-15 23:31:55 -0500

    Successfully got restapi to communicate with local db, added post user and get all to test functionality

--- rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
+++ rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
@@ -1,43 +1,69 @@
 package com.neur.app.rest.Controller;
 
+import com.neur.app.rest.Models.User;
+import com.neur.app.rest.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApiControllers {
 
-    @PostMapping(value = "/auth/signup")
-    public String createAccount() {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping(value = "/")
+    public String getPage() {
         return "Hello World";
     }
 
-    @PostMapping(value = "/auth/login")
-    public String loginAccount() {
-        return "Hello World";
+    @GetMapping(value = "/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
     }
 
-    @PostMapping(value = "/auth/logout")
-    public String logoutAccount() {
-        return "Hello World";
+    @PostMapping(value = "/users")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved!";
     }
 
-    @PostMapping(value = "/auth/forgot-password")
-    public String forgotPassword() {
-        return "Hello World";
-    }
-
-    @PostMapping(value = "/auth/reset-password")
-    public String resetPassword() {
-        return "Hello World";
-    }
-
-    @PostMapping(value = "/auth/reset-password")
-    public String refreshToken() {
-        return "Hello World";
-    }
-
-    @PostMapping(value = "/auth/verify-email")
-    public String verifyEmail() {
-        return "Hello World";
-    }
+//    @PostMapping(value = "/auth/signup")
+//    public String createAccount() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/login")
+//    public String loginAccount() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/logout")
+//    public String logoutAccount() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/forgot-password")
+//    public String forgotPassword() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/reset-password")
+//    public String resetPassword() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/reset-password")
+//    public String refreshToken() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping(value = "/auth/verify-email")
+//    public String verifyEmail() {
+//        return "Hello World";
+//    }
 }
