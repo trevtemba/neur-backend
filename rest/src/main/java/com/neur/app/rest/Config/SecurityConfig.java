8ba4eaf8a174128e90cbb0adbba45ccedfd34074
Temba <drxpnotlox@gmail.com>
2024-11-23 17:53:16 -0500

    Added dependencies for JWT integration (chose jjwt, doesnt have external dependencies, and seems to be industry standard in spring apps), also instantiated AuthenticationManager bean to manage JWT integration

--- rest/src/main/java/com/neur/app/rest/Config/SecurityConfig.java
+++ rest/src/main/java/com/neur/app/rest/Config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.neur.app.rest.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -67,12 +69,19 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsManager(user1, user2);
 //    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+
+        return config.getAuthenticationManager();
+
+    }
+
     // We want to make our own authentication provider...
     @Bean
     public AuthenticationProvider authenticationProvider() {
         // We choose to use the Dao Authentication provider, which needs...
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        // A password encoder...
+        // A password encoder, was bcrypt, switched to argon2...
         provider.setPasswordEncoder(new Argon2PasswordEncoder(
                 16, 32, 1, 65536, 3));
         // And userDetailsService, this is an interface, so we implement it in services->MyUserDetailsService
