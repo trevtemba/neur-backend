7c6160dc53f30a5171e87d15cf43b3df5e128b7d
Temba <drxpnotlox@gmail.com>
2025-01-01 20:37:23 -0500

    removed auth service dead code (already refactored into user service)

--- rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
+++ rest/src/main/java/com/neur/app/rest/Controller/ApiControllers.java
@@ -55,38 +55,6 @@ public class ApiControllers {
     public String deleteUser(@PathVariable long id) {
         return userService.deleteUser(id);
     }
-//    @PostMapping(value = "/auth/signup")
-//    public String createAccount() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/login")
-//    public String loginAccount() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/logout")
-//    public String logoutAccount() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/forgot-password")
-//    public String forgotPassword() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/reset-password")
-//    public String resetPassword() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/reset-password")
-//    public String refreshToken() {
-//        return "Hello World";
-//    }
-//
-//    @PostMapping(value = "/auth/verify-email")
-//    public String verifyEmail() {
-//        return "Hello World";
-//    }
+
+
 }
